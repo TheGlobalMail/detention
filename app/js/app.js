d6eeab56c611ec2e09636a898ab97b25623d8f15
Mark Finger <markfinger@gmail.com>
2013-04-23 13:25:01 +1000

    Added a mockup of the grid layout for the incident data.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,5 @@
-(function() {
+tgm = (function() {
   'use strict';
-
-  $(renderGraphs);
 
   function renderGraphs(){
     var idcs = ['north-west-point-immigration-facility', 'curtin-idc', 'perth-idc', 'villawood-idc', 'scherger-idc',
@@ -25,4 +23,69 @@
     graphs.append($('<img src="graphs/total-population-by-nationality.png">'));
   }
 
-}());
+  function getProcessedIncidentData() {
+    var occuredOn = tgm.data.incidents["Occurred On"];
+    var types = tgm.data.incidents["Type"];
+    var dates = _(occuredOn)
+      .unique()
+      .sortBy(function(date) {
+        return new Date(date.split('/'));
+      }).value();
+    var counts = _.countBy(occuredOn, _.identity);
+    return {
+      // Ordered list of dates
+      dates: dates,
+      // Map of date -> incident count
+      counts: counts
+    };
+    // {
+    //   "1909/2/31": ["Some incident", ...],
+    //   ...
+    // }
+  }
+
+  function buildIncidentGrid() {
+    var container = $('.incident-grid');
+    var data = getProcessedIncidentData();
+    var i = 0;
+    var addDateToGrid = function(date) {
+      // Delay for an increasingly larger time period so that
+      // we don't halt the browser
+      setTimeout(function() {
+        var dateClass = date.replace(/\//g, '-'); // Replace slashes with dashes
+        container.append('<div class="date ' + dateClass +'">');
+        var dateElement = container.find('.' + dateClass);
+        _(data.counts[date])
+          .times(function(i) {
+            dateElement.append('<div class="cell">');
+          }).tap(function() {
+            dateElement.append('<div class="clear">');
+          });
+      }, i++)
+    };
+    _.each(data.dates.slice(0, 250), addDateToGrid)
+
+    // Set some random ones as active
+    setTimeout(function() {
+      var cells = container.find('.cell');
+      for(var i = 0; i < 50; i++) {
+        var cell = cells[Math.floor(Math.random() * cells.length)];
+        $(cell).addClass('active');
+      }
+    }, 1000);
+  }
+
+  function init() {
+    renderGraphs();
+    buildIncidentGrid();
+  }
+
+  return {
+    init: init,
+    debug: function() {
+      debugger
+    }
+  }
+})();
+
+$(tgm.init);
