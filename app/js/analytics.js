12e32fb84a8b4823ca352241d888884045c2c411
Mark Finger <markfinger@gmail.com>
2013-04-23 14:22:57 +1000

    Shuffling things around and splitting the charts from the app mockup.

---
+++ app/js/analytics.js
@@ -0,0 +1,38 @@
+tgm = (function() {
+  'use strict';
+
+  function renderGraphs(){
+    var idcs = ['north-west-point-immigration-facility', 'curtin-idc', 'perth-idc', 'villawood-idc', 'scherger-idc',
+      'maribyrnong-idc', 'northern-idc'].sort();
+    var facilities = ['overall'].concat(idcs).concat(['domestic', 'offshore']);
+
+    var graphs = $('#incident-category-graphs');
+    graphs.html(_.map(facilities, function(graph){
+      var html = ''; //'<h3>' + idc + '</h3>';
+      html += '<img src="graphs/' + graph + '-incident-categories.png" />';
+      if (!graph.match(/domestic|offshore/)){
+        html += '<img class="incident-population" src="graphs/' + graph + '-population.png" />';
+        html += '<img class="incidents-vs-population" src="graphs/' + graph + '-population-vs-incidents.png" />';
+      }
+      if (graph.match(/villawood|curtin|nothern|north-west/i)){
+        html += '<img class="incident-contraband" src="graphs/' + graph + '-contraband-incident-categories.png" />';
+      }
+      return html;
+    }).join('\n'));
+    
+    graphs.append($('<img src="graphs/total-population-by-nationality.png">'));
+  }
+
+  function init() {
+    renderGraphs();
+  }
+
+  return {
+    init: init,
+    debug: function() {
+      debugger
+    }
+  }
+})();
+
+$(tgm.init);
--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,9 @@
-tgm = (function() {
+tgm = window.tgm || {};
+tgm.app = (function() {
   'use strict';
 
-  function renderGraphs(){
-    var idcs = ['north-west-point-immigration-facility', 'curtin-idc', 'perth-idc', 'villawood-idc', 'scherger-idc',
-      'maribyrnong-idc', 'northern-idc'].sort();
-    var facilities = ['overall'].concat(idcs).concat(['domestic', 'offshore']);
-
-    var graphs = $('#incident-category-graphs');
-    graphs.html(_.map(facilities, function(graph){
-      var html = ''; //'<h3>' + idc + '</h3>';
-      html += '<img src="graphs/' + graph + '-incident-categories.png" />';
-      if (!graph.match(/domestic|offshore/)){
-        html += '<img class="incident-population" src="graphs/' + graph + '-population.png" />';
-        html += '<img class="incidents-vs-population" src="graphs/' + graph + '-population-vs-incidents.png" />';
-      }
-      if (graph.match(/villawood|curtin|nothern|north-west/i)){
-        html += '<img class="incident-contraband" src="graphs/' + graph + '-contraband-incident-categories.png" />';
-      }
-      return html;
-    }).join('\n'));
-    
-    graphs.append($('<img src="graphs/total-population-by-nationality.png">'));
-  }
+  var gridContainer = $('.incident-grid');
+  var data;
 
   function getProcessedIncidentData() {
     var occuredOn = tgm.data.incidents["Occurred On"];
@@ -38,23 +20,18 @@ tgm = (function() {
       // Map of date -> incident count
       counts: counts
     };
-    // {
-    //   "1909/2/31": ["Some incident", ...],
-    //   ...
-    // }
   }
 
-  function buildIncidentGrid() {
-    var container = $('.incident-grid');
-    var data = getProcessedIncidentData();
+  function buildIncidentDayGrid() {
+    gridContainer.find('.cell').remove();
     var i = 0;
     var addDateToGrid = function(date) {
       // Delay for an increasingly larger time period so that
       // we don't halt the browser
       setTimeout(function() {
         var dateClass = date.replace(/\//g, '-'); // Replace slashes with dashes
-        container.append('<div class="date ' + dateClass +'">');
-        var dateElement = container.find('.' + dateClass);
+        gridContainer.append('<div class="date ' + dateClass +'">');
+        var dateElement = gridContainer.find('.' + dateClass);
         _(data.counts[date])
           .times(function(i) {
             dateElement.append('<div class="cell">');
@@ -63,29 +40,67 @@ tgm = (function() {
           });
       }, i++)
     };
-    _.each(data.dates.slice(0, 250), addDateToGrid)
+
+    _.each(data.dates, addDateToGrid);
 
     // Set some random ones as active
     setTimeout(function() {
-      var cells = container.find('.cell');
-      for(var i = 0; i < 50; i++) {
+      var cells = gridContainer.find('.cell');
+      for(var i = 0; i < Math.floor(cells.length / 20); i++) {
         var cell = cells[Math.floor(Math.random() * cells.length)];
         $(cell).addClass('active');
       }
     }, 1000);
   }
 
+  function buildIncidentMonthGrid() {
+    var i = 0;
+    var addDateToGrid = function(date) {
+      // Delay for an increasingly larger time period so that
+      // we don't halt the browser
+      setTimeout(function() {
+        var dateClass = date.replace(/\//g, '-'); // Replace slashes with dashes
+        gridContainer.append('<div class="date ' + dateClass +'">');
+        var dateElement = gridContainer.find('.' + dateClass);
+        _(data.counts[date])
+          .times(function(i) {
+            dateElement.append('<div class="cell">');
+          }).tap(function() {
+            dateElement.append('<div class="clear">');
+          });
+      }, i++)
+    };
+
+    _.each(data.dates, addDateToGrid);
+
+    // Set some random ones as active
+    setTimeout(function() {
+      var cells = gridContainer.find('.cell');
+      for(var i = 0; i < Math.floor(cells.length / 20); i++) {
+        var cell = cells[Math.floor(Math.random() * cells.length)];
+        $(cell).addClass('active');
+      }
+    }, 1000);
+  }
+
+  function bindControls() {
+    var controls = $('.controls');
+
+    controls.find('.day')
+      .on('click', buildIncidentDayGrid);
+    controls.find('.month')
+      .on('click', buildIncidentMonthGrid);
+  }
+
   function init() {
-    renderGraphs();
-    buildIncidentGrid();
+    data = getProcessedIncidentData();
+    buildIncidentDayGrid();
+    bindControls();
   }
 
   return {
-    init: init,
-    debug: function() {
-      debugger
-    }
+    init: init
   }
 })();
 
-$(tgm.init);
+$(tgm.app.init);
