daa2aa62d558e77be3cdaf3345652c0f4597fd61
Mark Finger <markfinger@gmail.com>
2013-04-29 17:20:21 +1000

    Improving the HTML mockup.

--- app/js/app/main.js
+++ app/js/app/main.js
@@ -1,114 +1,45 @@
 define([
   'jquery',
   'lodash',
-  'data/incidents'
-], function($, _, incidents) {
+  './processed-data'
+], function($, _, data) {
   'use strict';
 
   var gridContainer = $('.incident-grid');
-  var data;
-
-  function processDateStrings() {
-    incidents["Occurred On"] = _.map(
-      incidents["Occurred On"],
-      function(date) {
-        // `date` takes the format MM/DD/YYYY
-
-        var split = date.split('/');
-        var normal = [split[2], split[0], split[1]].join('/');
-        var unix = (new Date(normal.split('/'))).getTime();
-        var dateClass = normal.replace(/\//g, '-'); // Replace slashes with dashes
-        var month = normal.split('/').slice(0,2).join('/');
-
-        return {
-          original: date,
-          normal: normal,
-          unix: unix,
-          dateClass: dateClass,
-          month: month
-        }
-      }
-    )
-  }
-
-  function processIncidentData() {
-    var occurredOn = incidents["Occurred On"];
-
-    var dates = _(occurredOn)
-      .unique('original')
-      .sortBy('unix')
-      .value();
-    var dateCounts = _.countBy(occurredOn, 'original');
-
-    var months = _.unique(dates, 'month');
-    var monthCounts = _.countBy(occurredOn, 'month');
-
-    data = {
-      dates: dates,
-      dateCounts: dateCounts,
-      months: months,
-      monthCounts: monthCounts
-    };
-  }
-
-  function buildIncidentDayGrid() {
-    resetGridContainer();
-
-    _.each(
-      data.dates,
-      getAddDateToGrid(
-        data.dateCounts,
-        'original'
-      )
-    );
-
-    // Set some random ones as active
-    setTimeout(function() {
-      var cells = gridContainer.find('.cell');
-      for(var i = 0; i < Math.floor(cells.length / 20); i++) {
-        var cell = cells[Math.floor(Math.random() * cells.length)];
-        $(cell).addClass('active');
-      }
-    }, 1000);
-  }
 
   function buildIncidentMonthGrid() {
     resetGridContainer();
 
-    _.each(
+    var countData = data.monthCounts;
+    var dateKey = 'month';
+    var rowElements = _.map(
       data.months,
-      getAddDateToGrid(
-        data.monthCounts,
-        'month'
-      )
-    );
-
-    // Set some random ones as active
-    setTimeout(function() {
-      var cells = gridContainer.find('.cell');
-      for(var i = 0; i < Math.floor(cells.length / 50); i++) {
-        var cell = cells[Math.floor(Math.random() * cells.length)];
-        $(cell).addClass('active');
-      }
-    }, 1000);
-  }
-
-  function getAddDateToGrid(countData, dateKey) {
-    var i = 0;
-    return function addDateToGrid(date) {
-      // Delay for an increasingly larger time period so that
-      // we don't halt the browser
-      setTimeout(function() {
-        gridContainer.append('<div class="date ' + date.dateClass +'">');
-        var rowElement = gridContainer.find('.' + date.dateClass);
+      function(date) {
+        var rowElement = $('<div class="date ' + date.dateClass +'">');
         _(countData[date[dateKey]])
           .times(function(i) {
             rowElement.append('<div class="cell">');
           }).tap(function() {
             rowElement.append('<div class="clear">');
           });
-      }, i++);
-    }
+        return rowElement;
+      }
+    );
+
+    _.each(rowElements, function(element, i) {
+      setTimeout(function() {
+        gridContainer.append(element)
+      }, i * 100);
+    });
+
+    // Set some random ones as active
+    setTimeout(function() {
+      var cells = gridContainer.find('.cell');
+      for (var i = 0; i < Math.floor(cells.length / 50); i++) {
+        var cell = cells[Math.floor(Math.random() * cells.length)];
+        $(cell).addClass('active');
+      }
+    }, rowElements.length * 100);
   }
 
   function resetGridContainer() {
@@ -118,16 +49,16 @@ define([
   function bindControls() {
     var controls = $('.controls');
 
-    controls.find('.day')
-      .on('click', buildIncidentDayGrid);
     controls.find('.month')
       .on('click', buildIncidentMonthGrid);
+
+    gridContainer.on('click touch', '.date .cell', function() {
+      $(this).toggleClass('active');
+    })
   }
 
   function init() {
-    processDateStrings();
-    processIncidentData();
-    buildIncidentDayGrid();
+    buildIncidentMonthGrid();
     bindControls();
   }
 
---
+++ app/js/app/processed-data.js
@@ -0,0 +1,53 @@
+define([
+  'lodash',
+  '../data/incidents'
+], function(_, incidents) {
+  "use strict";
+
+  function processDateStrings() {
+    incidents["Occurred On"] = _.map(
+      incidents["Occurred On"],
+      function(date) {
+        // `date` takes the format MM/DD/YYYY
+
+        var split = date.split('/');
+        var normal = [split[2], split[0], split[1]].join('/');
+        var unix = (new Date(normal.split('/'))).getTime();
+        var dateClass = normal.replace(/\//g, '-'); // Replace slashes with dashes
+        var month = normal.split('/').slice(0,2).join('/');
+
+        return {
+          original: date,
+          normal: normal,
+          unix: unix,
+          dateClass: dateClass,
+          month: month
+        }
+      }
+    )
+  }
+
+  function processIncidentData() {
+    processDateStrings();
+
+    var occurredOn = incidents["Occurred On"];
+
+    var dates = _(occurredOn)
+      .unique('original')
+      .sortBy('unix')
+      .value();
+    var dateCounts = _.countBy(occurredOn, 'original');
+
+    var months = _.unique(dates, 'month');
+    var monthCounts = _.countBy(occurredOn, 'month');
+
+    return {
+      dates: dates,
+      dateCounts: dateCounts,
+      months: months,
+      monthCounts: monthCounts
+    };
+  }
+
+  return processIncidentData();
+});
