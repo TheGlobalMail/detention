cb4ad93ebbf594e9f7c6447efe0c4f596fa23fc7
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-06-11 16:28:15 +1000

    Add fb url rewrite

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var fs = require("fs");
 var express = require("express");
 var site = express();
 
-// Serve static files
-site.use("/", express.static(__dirname + '/dist'));
+var indexHtml = fs.readFileSync(__dirname + "/dist/index.html", 'utf8');
+var fbMetaRegex = /"og:url" content="http:\/\/behindthewire.theglobalmail.org/;
 
+// Serve static files
 site.get("/test-story/*", function(req, res) {
   fs.createReadStream(__dirname + "/dist/story.html").pipe(res);
 });
@@ -18,9 +19,17 @@ site.get("/embed/*", function(req, res) {
   fs.createReadStream(__dirname + "/dist/embed.html").pipe(res);
 });
 
+site.get("/", function(req, res) {
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write(indexHtml);
+  res.end();
+});
+
 // Ensure all routes go home, client side app..
 site.get("*", function(req, res) {
-  fs.createReadStream(__dirname + "/dist/index.html").pipe(res);
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write(indexHtml.replace(fbMetaRegex, '$&' + req.url));
+  res.end();
 });
 
 // Actually listen
