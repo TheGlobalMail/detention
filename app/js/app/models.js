5e619e3c22753f6a5b89049dd1e09a09306f8e21
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-06-12 10:44:30 +1000

    Fix to adopt an incident request link

--- app/js/app/models.js
+++ app/js/app/models.js
@@ -792,7 +792,7 @@ define([
         _this.data.id +
         ';body=To%20the%20Department%20of%20Immigration%20and%20Citizenship%2C%0A%0ADear%20Sir%2FMadam%2C%0A%0AU' +
         'nder%20the%20Freedom%20of%20Information%20Act%201982%20(Cth)%20I%20request%20the%20following%20document' +
-        '%3A%0A%0AIncident%20Detail%20Report%201-2UMYBH%20from%20the%20Department%27s%20Compliance%2C%20Case%20M' +
+        '%3A%0A%0AIncident%20Detail%20Report%20' + _this.data.id + '%20from%20the%20Department%27s%20Compliance%2C%20Case%20M' +
         'anagement%2C%20Detention%20and%20Settlement%20Portal.%20I%20also%20request%20any%20documents%20attached' +
         '%20to%20the%20detailed%20report.%0A%0AKind%20Regards%2C%0A%0A****ADD%20YOUR%20NAME%20HERE%20BEFORE%20SE' +
         'NDING%20REQUEST****';
